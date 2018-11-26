1d538a27ee220544b5b5b23d87f9d2da687fc5e9
ambat25 <25ambat25@gmail.com>
2018-11-26 15:24:26 +0100

    moved functionality of ProcessSheet to GoogleSheetService

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const moment = require('moment');
+const Promise = require('bluebird');
 const github = require('octonode');
-const { google } = require('googleapis');
+const {
+  google,
+} = require('googleapis');
+const GoogleSpreadsheet = require('google-spreadsheet');
 const GithubService = require('./classes/GithubService');
 const GoogleSheetService = require('./classes/GoogleSheetService');
-const processSheet = require('./ProcessSheet');
 const {
   repos,
   githubToken,
@@ -11,21 +14,22 @@ const {
   sheetId,
 } = require('./config.json');
 
-
 async function main() {
-  await processSheet(sheetId, sheetAuth);
+  const doc = Promise.promisifyAll(new GoogleSpreadsheet(sheetId));
+
   const objGoogleAuthentication = new google.auth.JWT(
     sheetAuth.client_email,
     null,
     sheetAuth.private_key,
     'https://www.googleapis.com/auth/spreadsheets',
   );
+
   const githubService = new GithubService(github, repos, githubToken, moment);
   const googleSheetService = new GoogleSheetService(
-    google.sheets,
-    objGoogleAuthentication,
-    sheetId,
+    google.sheets, objGoogleAuthentication, sheetId, doc, sheetAuth,
   );
+  // Returns the GID
+  await googleSheetService.makeSheet();
   const getPullRequests = await githubService.getRepositories();
   await googleSheetService.postPullRequests(getPullRequests);
 }
